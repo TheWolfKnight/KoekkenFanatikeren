55bcedc400a83c1e476f79b59869f6bbd6dd2881
LAPTOP-SUJO5FFU\danni <dannieschytte@gmail.com>
2022-11-17 11:11:39 +0100

    c# UI-design

--- KøkkenFanatikeren/Form1.Designer.cs
+++ KøkkenFanatikeren/Form1.Designer.cs
@@ -29,13 +29,245 @@ namespace KøkkenFanatikeren
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tb_EmployeName = new System.Windows.Forms.TextBox();
+            this.tb_password = new System.Windows.Forms.TextBox();
+            this.tb_customerName = new System.Windows.Forms.TextBox();
+            this.tb_CustomerID = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.b_login = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.b_print = new System.Windows.Forms.Button();
+            this.b_save = new System.Windows.Forms.Button();
+            this.b_edit = new System.Windows.Forms.Button();
+            this.b_greatNewDoc = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Employe";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Customer ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Customer Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Password";
+            // 
+            // tb_EmployeName
+            // 
+            this.tb_EmployeName.Location = new System.Drawing.Point(137, 3);
+            this.tb_EmployeName.Name = "tb_EmployeName";
+            this.tb_EmployeName.Size = new System.Drawing.Size(100, 22);
+            this.tb_EmployeName.TabIndex = 4;
+            // 
+            // tb_password
+            // 
+            this.tb_password.Location = new System.Drawing.Point(137, 28);
+            this.tb_password.Name = "tb_password";
+            this.tb_password.Size = new System.Drawing.Size(100, 22);
+            this.tb_password.TabIndex = 5;
+            // 
+            // tb_customerName
+            // 
+            this.tb_customerName.Location = new System.Drawing.Point(137, 62);
+            this.tb_customerName.Name = "tb_customerName";
+            this.tb_customerName.Size = new System.Drawing.Size(100, 22);
+            this.tb_customerName.TabIndex = 6;
+            // 
+            // tb_CustomerID
+            // 
+            this.tb_CustomerID.Location = new System.Drawing.Point(137, 88);
+            this.tb_CustomerID.Name = "tb_CustomerID";
+            this.tb_CustomerID.Size = new System.Drawing.Size(100, 22);
+            this.tb_CustomerID.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(76, 145);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(108, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Your Assignment";
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(15, 173);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(222, 283);
+            this.listView1.TabIndex = 9;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(243, 173);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(545, 265);
+            this.tabControl1.TabIndex = 10;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(537, 236);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(192, 71);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // b_login
+            // 
+            this.b_login.Location = new System.Drawing.Point(243, 12);
+            this.b_login.Name = "b_login";
+            this.b_login.Size = new System.Drawing.Size(92, 39);
+            this.b_login.TabIndex = 11;
+            this.b_login.Text = "Login";
+            this.b_login.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(243, 68);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 39);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // b_print
+            // 
+            this.b_print.Location = new System.Drawing.Point(379, 12);
+            this.b_print.Name = "b_print";
+            this.b_print.Size = new System.Drawing.Size(92, 39);
+            this.b_print.TabIndex = 13;
+            this.b_print.Text = "Print";
+            this.b_print.UseVisualStyleBackColor = true;
+            // 
+            // b_save
+            // 
+            this.b_save.Location = new System.Drawing.Point(486, 12);
+            this.b_save.Name = "b_save";
+            this.b_save.Size = new System.Drawing.Size(92, 39);
+            this.b_save.TabIndex = 14;
+            this.b_save.Text = "Save";
+            this.b_save.UseVisualStyleBackColor = true;
+            // 
+            // b_edit
+            // 
+            this.b_edit.Location = new System.Drawing.Point(584, 12);
+            this.b_edit.Name = "b_edit";
+            this.b_edit.Size = new System.Drawing.Size(92, 39);
+            this.b_edit.TabIndex = 15;
+            this.b_edit.Text = "Edit";
+            this.b_edit.UseVisualStyleBackColor = true;
+            // 
+            // b_greatNewDoc
+            // 
+            this.b_greatNewDoc.Location = new System.Drawing.Point(682, 12);
+            this.b_greatNewDoc.Name = "b_greatNewDoc";
+            this.b_greatNewDoc.Size = new System.Drawing.Size(92, 39);
+            this.b_greatNewDoc.TabIndex = 16;
+            this.b_greatNewDoc.Text = "New";
+            this.b_greatNewDoc.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.b_greatNewDoc);
+            this.Controls.Add(this.b_edit);
+            this.Controls.Add(this.b_save);
+            this.Controls.Add(this.b_print);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.b_login);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tb_CustomerID);
+            this.Controls.Add(this.tb_customerName);
+            this.Controls.Add(this.tb_password);
+            this.Controls.Add(this.tb_EmployeName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        public System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        public System.Windows.Forms.Label label4;
+        public System.Windows.Forms.TextBox tb_EmployeName;
+        private System.Windows.Forms.ListView listView1;
+        public System.Windows.Forms.TextBox tb_password;
+        public System.Windows.Forms.TextBox tb_customerName;
+        public System.Windows.Forms.TextBox tb_CustomerID;
+        public System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        public System.Windows.Forms.Button b_login;
+        public System.Windows.Forms.Button button1;
+        public System.Windows.Forms.Button b_print;
+        public System.Windows.Forms.Button b_save;
+        public System.Windows.Forms.Button b_edit;
+        public System.Windows.Forms.Button b_greatNewDoc;
     }
 }
 
