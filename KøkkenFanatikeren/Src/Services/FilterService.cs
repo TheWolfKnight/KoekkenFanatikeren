964d025e76f3bc16fa6d39e5cd7619fd31a8f41e
Martin <mtda49483@edu.ucl.dk>
2022-11-18 10:17:46 +0100

    Testing filter (will be heavily changed or deleted)

---
+++ KøkkenFanatikeren/Src/Services/FilterService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KøkkenFanatikeren.Src.Models;
+
+namespace KøkkenFanatikeren.Src.Services
+{
+    public class FilterService
+    {
+        public List<TestItem> SortItemsByPrice(List<TestItem> listToSort)
+        {
+            List <TestItem> SortedList = listToSort.OrderBy(o => o.UnitPrice).ToList();
+            return SortedList;
+        }
+    }
+}
