a6078418c196330d02006b044cb49c6052815dbd
Philip Knudsen <philipalexkndusen@gmail.com>
2022-11-21 11:41:12 +0100

    ibid

--- KøkkenFanatikeren/Form1.cs
+++ KøkkenFanatikeren/Form1.cs
@@ -8,8 +8,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-using KøkkenFanatikeren.Src.Repository;
-
 namespace KøkkenFanatikeren
 {
     public partial class Form1 : Form
