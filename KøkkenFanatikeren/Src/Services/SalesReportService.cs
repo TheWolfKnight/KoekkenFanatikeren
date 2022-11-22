b4a7f1336e17d8daba943ac2539ddc2f4be363ea
Philip Knudsen <philipalexkndusen@gmail.com>
2022-11-22 10:07:19 +0100

    added the fileds Completion and Creation to the Orders database, added the file SalesReportService.cs to the service folder

---
+++ KøkkenFanatikeren/Src/Services/SalesReportService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KøkkenFanatikeren.Src.Services
+{
+    public class SalesReportService
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public SalesReportService()
+        {
+
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="orders"></param>
+        /// <returns></returns>
+        public string[] GenerateReport( List<Models.Order> orders )
+        {
+
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <param name="totalPrice"></param>
+        /// <returns></returns>
+        public string[] GenerateFileHeadersAndFooter(DateTime start, DateTime end, double totalPrice)
+        {
+            // Writes the header and footer into the respective variables
+            string header = $"SALGSTATISTIK {end.Year}\tfra dato {start:d} til dato {end:d}";
+            string footer = $"i alt {totalPrice}";
+
+            // Returns the header and footer as the function result
+            return new string[] { header, footer };
+        }
+
+    }
+}
