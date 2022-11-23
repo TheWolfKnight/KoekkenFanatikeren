b16694121a8215868a6e81e0a7b1d7ae3aa441d3
Martin <mtda49483@edu.ucl.dk>
2022-11-23 10:57:40 +0100

    New sorting & added overall documentation

---
+++ KøkkenFanatikeren/Frontend/Testing.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using KøkkenFanatikeren.Src.Models;
+using KøkkenFanatikeren.Src.Database;
+using KøkkenFanatikeren.Src.Services;
+using Item = KøkkenFanatikeren.Src.Models.Item;
+
+namespace KøkkenFanatikeren.Frontend
+{
+    public partial class Testing : Form
+    {
+        public Testing()
+        {
+            InitializeComponent();
+            FilterService filterService = new FilterService();
+            List<Item> list = filterService.GetAllItems();
+            List<Item> QuantitySort = filterService.SortByQuantity(true);
+            List<Item> PriceSort = filterService.SortByPrice(50, 1000, true);
+            List<Item> CategorySort = filterService.SortByCategory(2);
+
+            Src.Database.DataClasses1DataContext dbContext = new DataClasses1DataContext();
+
+            //List<Item> Sort = filterService.Sort();
+
+            foreach (Item iteminlist in PriceSort)
+            {
+                Console.WriteLine(iteminlist.UnitPrice.ToString());
+            }
+
+            //foreach (int item in DimensionSort)
+            //{
+            //    MessageBox.Show(item.ToString());
+            //}
+        }
+
+        private void Testing_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- KøkkenFanatikeren/Src/Services/FilterService.cs
+++ KøkkenFanatikeren/Src/Services/FilterService.cs
@@ -11,23 +11,47 @@ namespace KøkkenFanatikeren.Src.Services
 {
     public class FilterService
     {
-        public List<Models.Item> SortByPrice(int min, int max)
+        /// <summary>
+        /// Sorts all items by the given minimum and maximum price and ordered by ascending bool
+        /// </summary>
+        /// <param name="min"> Minimum int to search for </param>
+        /// <param name="max"> Maximum int to search for </param>
+        /// <param name="Ascending"> Orderby bool </param>
+        public List<Models.Item> SortByPrice(int min, int max, bool Ascending)
         {
-           Repository.Repo filterRepository = new Repo();
-            return filterRepository.SortByPrice(min, max);
+           Repository.FilterRepository filterRepository = new FilterRepository();
+            return filterRepository.SortByPrice(min, max, Ascending);
         }
 
+        /// <summary>
+        /// Sorts all items by quantitiy in order by given ascending bool
+        /// </summary>
+        /// <param name="Ascending"> Orderby bool </param>
         public List<Models.Item> SortByQuantity(bool Ascending)
         {
-            Repository.Repo filterRepository = new Repo();
+            Repository.FilterRepository filterRepository = new FilterRepository();
             return filterRepository.SortByQuantity(Ascending);
         }
 
+        /// <summary>
+        /// retrieves all items in the database
+        /// </summary>
         public List<Models.Item> GetAllItems()
         {
-            Repository.Repo filterRepository = new Repo();
+            Repository.FilterRepository filterRepository = new FilterRepository();
             return filterRepository.GetAllItems();
         }
+
+        /// <summary>
+        /// Sorts all items by the categoryId, which determines material. 
+        /// </summary>
+        /// <param name="CategoryId"> Int CategoryId (foreign key) </param>
+        public List<Models.Item> SortByCategory(int CategoryId)
+        {
+            Repository.FilterRepository filterRepository = new FilterRepository();
+            return filterRepository.SortByCategory(CategoryId);
+        }
+
 
     }
 }
